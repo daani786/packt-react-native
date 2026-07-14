dddcb7ed3328ba8cae57fdd1d398eb16d385fb9f
Muhammad Adnan Masood <m.adnan.masood@gmail.com>
2026-07-14 18:48:18 +0500

    remove alignments and extra text

--- code/App.tsx
+++ code/App.tsx
@@ -3,8 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <Text style={styles.text}>Welcome to my first app</Text>
+      <Text>Hello, World</Text>
     </View>
   );
 }
@@ -12,11 +11,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1, // area of screen that the component should take up
-    backgroundColor: 'pink',
-    alignItems: 'center', // horizontal alignment
-    justifyContent: 'center' // vertical alignment
+    backgroundColor: 'white',
   },
-  text: {
-    color: 'red'
-  }
 });
