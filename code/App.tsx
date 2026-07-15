e4d490ba08f0e477f824d35d6082f41fcbc19267
Muhammad Adnan Masood <m.adnan.masood@gmail.com>
2026-07-15 16:50:51 +0500

    use of ActivityIndicator

--- code/App.tsx
+++ code/App.tsx
@@ -1,11 +1,12 @@
-import { Alert, StyleSheet, Text, Platform } from "react-native";
+import { Alert, StyleSheet, Text, ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Hello, World!</Text>
-      <Text>This is an {Platform.OS === "android" ? "Android" : "iOS"} Device</Text>
+      <ActivityIndicator/>
+      <ActivityIndicator size="large" color="red" />
     </SafeAreaView>
   );
 }
@@ -14,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, // area of screen that the component should take up
     backgroundColor: "white",
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
   title: {
     fontSize: 30,
