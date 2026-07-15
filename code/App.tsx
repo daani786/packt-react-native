77ce61294c1cb7dbd7dad56904d5b5654b784c8b
Muhammad Adnan Masood <m.adnan.masood@gmail.com>
2026-07-15 16:26:33 +0500

    use of scrollview

--- code/App.tsx
+++ code/App.tsx
@@ -1,4 +1,4 @@
-import { Alert, StyleSheet, Text, Image, Button, TouchableOpacity, Pressable } from "react-native";
+import { Alert, StyleSheet, Text, Image, Button, TouchableOpacity, Pressable, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
@@ -8,30 +8,51 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>My App</Text>
-      <Image 
-        source={require("./assets/splash-icon.png")}
-        style={styles.image1}
-      />
-      <Pressable onPress={onButtonPress}>
-        <Image
-          source={{ uri: "https://picsum.photos/200/300" }}
-          style={styles.image2}
-          borderRadius={100}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        // horizontal={true}
+        contentContainerStyle={{ alignItems: "center", backgroundColor: "red" }}
+      >
+        <Text style={styles.title}>My App</Text>
+        <Image 
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
         />
-      </Pressable>
-      <TouchableOpacity onPress={onButtonPress}>
-        <Image
-          source={{ uri: "https://picsum.photos/200/300" }}
-          style={styles.image2}
-          borderRadius={100}
-        />
-      </TouchableOpacity>
+        <Pressable onPress={onButtonPress}>
+          <Image
+            source={{ uri: "https://picsum.photos/200/300" }}
+            style={styles.image2}
+            borderRadius={100}
+          />
+        </Pressable>
+        <TouchableOpacity onPress={onButtonPress}>
+          <Image
+            source={{ uri: "https://picsum.photos/200/300" }}
+            style={styles.image2}
+            borderRadius={100}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onButtonPress}>
+          <Image
+            source={{ uri: "https://picsum.photos/200/300" }}
+            style={styles.image2}
+            borderRadius={100}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onButtonPress}>
+          <Image
+            source={{ uri: "https://picsum.photos/200/300" }}
+            style={styles.image2}
+            borderRadius={100}
+          />
+        </TouchableOpacity>
 
-      <Button 
-        title="Press on Me" 
-        onPress={onButtonPress}
-      />
+        <Button 
+          title="Press on Me" 
+          onPress={onButtonPress}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 }
