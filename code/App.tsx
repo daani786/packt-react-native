a9a9c562d8f69bab2d9a5a8c9210a3cd9ec15018
Muhammad Adnan Masood <m.adnan.masood@gmail.com>
2026-07-15 16:56:42 +0500

    use of View

--- code/App.tsx
+++ code/App.tsx
@@ -1,12 +1,17 @@
-import { Alert, StyleSheet, Text, ActivityIndicator } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Hello, World!</Text>
-      <ActivityIndicator/>
-      <ActivityIndicator size="large" color="red" />
+      <View style={styles.view1}>
+        <Text style={styles.text}>Hello</Text>
+        <Text style={styles.text}>Text 2</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>Text 3</Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -22,4 +27,19 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  text: {
+    fontSize: 50,
+  },
+  view1: {
+    height: 200,
+    width: 200,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  view2: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'aqua',
+  }
 });
