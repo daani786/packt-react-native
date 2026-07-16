b09029267c67a008ac7712511d82a92a485884e4
Muhammad Adnan Masood <m.adnan.masood@gmail.com>
2026-07-16 14:26:03 +0500

    use flex

--- code/App.tsx
+++ code/App.tsx
@@ -1,21 +1,17 @@
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Hello, World!</Text>
-      <View style={styles.view1}>
-        <Text style={styles.text}>Hello</Text>
-        <Text style={styles.text}>Text 2</Text>
-      </View>
-      <View style={styles.view2}>
-        <View style={styles.view2Child}>
-          <Text>Text 3</Text>
+      <View style={{ backgroundColor: "blue", flex: 1 }} >
+        <View style={{ backgroundColor: "red", flex: 1 }} >
+          <Text style={styles.title}>Hello, World!</Text>
         </View>
-      </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>Text 4</Text>
+        <View style={{ backgroundColor: "pink", flex: 1 }} >
+        </View>
+        <View style={{ backgroundColor: "green", flex: 1 }} >
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -32,31 +28,4 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  text: {
-    fontSize: 50,
-  },
-  view1: {
-    height: 200,
-    width: 200,
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  view2: {
-    height: 150,
-    width: 150,
-    backgroundColor: 'aqua',
-    marginVertical: 20,
-    padding: 10,
-  },
-  view2Child: {
-    height: 50,
-    width: 50,
-    backgroundColor: 'green',
-  },
-  view3: {
-    height: 200,
-    width: 200,
-    backgroundColor: 'pink',
-  }
 });
